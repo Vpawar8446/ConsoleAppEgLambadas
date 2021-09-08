1b811d3abef51894944fed8c2a1dd987352febe6
niles <niles@LAPTOP-3K24CQ1R>
2021-09-08 21:15:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleAppEgLambadas
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4, 5, 6 };
+            List<int> evenNumbers = list.FindAll(x => (x % 2) == 0);
+
+            foreach (var num in evenNumbers)
+            {
+                Console.Write("{0} ", num);
+            }
+            Console.WriteLine();
+            Console.Read();
+        }
+    }
+}
